b1aeb2494f8b63f62cfedb9b69f96f6f7d10c78b
Jason Garber <jason@sixtwothree.org>
2019-04-13 18:45:54 -0400

    Revert "Use load order as a stronger means of specifying validation application order"
    This reverts commit 45fe1609e022125db7f6ebb0e66e8d388eac849e.

--- lib/sinatra/param/validation.rb
+++ lib/sinatra/param/validation.rb
@@ -2,8 +2,12 @@ module Sinatra
   module Param
     class Validation
       class << self
-        def for_options(options)
-          subclasses.find_all { |validation| options.key?(validation.identifier) }
+        def for_options(required: nil, **options)
+          validations = subclasses.find_all { |validation| options.key?(validation.identifier) }
+
+          return validations unless required
+
+          validations.unshift(subclasses.find { |subclass| subclass.identifier == :required })
         end
 
         def identifier
